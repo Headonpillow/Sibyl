a76162d5dfed5738059786daaa8a003cd50bcedd
Headonpillow <assentato.lorenzo@gmail.com>
2025-02-07 15:11:51 +0100

    fixing checks

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,2 @@
 .onLoad <- function(libname, pkgname) {
-  suppressWarnings({
-    # Disable "rgl" creation of graphical windows, since we do not require
-    # this functionality from "geomorph".
-    options(rgl.useNULL = TRUE)
-  })
 }
